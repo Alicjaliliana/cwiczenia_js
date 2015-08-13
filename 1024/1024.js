9a57563e5d262f5cdbc6a60b88be81ba873bfa5a
Alicja Lilianna <Alicja0Wojcik@gmail.com>
2015-08-13 13:37:28 +0100

    created arrays

--- 1024/1024.js
+++ 1024/1024.js
@@ -13,7 +13,7 @@ function game1024(){
 		var tile = document.createElement('div');
 		tile.className = "tile";
 		tile.style.top = positionTop + "px";
-		tilesArray.push(tile);
+		tilesArray.push([]);
 		for (var counter = 0, positionLeft =0; counter < rows; counter ++, positionLeft +=110) {
 			var nexttile = document.createElement('div');
 			nexttile.className = "tile";
