55030a7cfd463ef70929c57748c07c99629a4803
Alicja Lilianna <Alicja0Wojcik@gmail.com>
2015-08-13 13:17:26 +0100

    finished tiles

--- 1024/1024.js
+++ 1024/1024.js
@@ -1,20 +1,27 @@
 function game1024(){
-	var counter = 0;
 	var rows = 4;
-	var position = 110;
+	var columns = 4;
 	var i = 0;
+	var positionTop = 0;
+	var tilesArray = []
+	
 	var main = document.createElement('div');
 	main.id = 'main';
 	document.getElementsByTagName('body')[0].appendChild(main);
 	
-	for (i; i < rows; i++){
-		while (counter < rows) {
-			var tile = document.createElement('div');
-			tile.className = "tile";
-			tile.style.left = position * counter + "px";
-			main.appendChild(tile);
-			counter ++
-		}	
-		tile.style.top = position *counter + "px";
+	for(i; i < columns; i++) {
+		var tile = document.createElement('div');
+		tile.className = "tile";
+		tile.style.top = positionTop + "px";
+		
+		for (var counter = 0, positionLeft =0; counter < rows; counter ++, positionLeft +=110) {
+			var nexttile = document.createElement('div');
+			nexttile.className = "tile";
+			nexttile.style.top = positionTop + "px";
+			nexttile.style.left = positionLeft + "px";
+			main.appendChild(nexttile);
+		}
+		main.appendChild(tile);
+		positionTop +=110;
 	}
 }
